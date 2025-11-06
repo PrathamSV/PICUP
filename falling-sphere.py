749490fd9b98fac4df5a878d54e0c5057dbd9a77
Pratham Vasisht <80379347+PrathamSV@users.noreply.github.com>
2025-11-06 23:29:21 +0530

    Refactor simulation and multi-sphere support
    Rewrote the simulation using a Ball class to encapsulate properties and methods for each sphere. Added support for simulating and plotting multiple spheres. Changed end condition to when sphere approaches terminal velocity.

--- falling-sphere.py
+++ falling-sphere.py
@@ -1,97 +1,159 @@
+"""Adapted from:
+Falling Sphere with Air Resistance Proportional to v^2 - https://doi.org/10.1119/PICUP.Exercise.fallsph"""
+
 from vpython import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Initial Conditions
+class Ball:
+    def __init__(self, radius, mass, initial_pos, vis_scalar, trail_color=color.gray(0.5), initial_vel=vector(0, 0, 0)):
+        self.radius = radius
+        self.mass = mass
+        self.initial_pos = initial_pos
+        self.velocity = initial_vel
+        self.vis_scalar = vis_scalar
+        self.trail_color = trail_color
+        self.drag_coeff = 0.5
+
+        self.vis_radius = radius * vis_scalar
+        self.cs_area = np.pi * radius**2
+
+        self.velocity = vector(0, 0, 0)
+
+        self.terminal_vel = self.get_terminal_velocity()
+        self.object = self.create_object()
+
+        self.comp_positions = []
+        self.time_steps = []
+
+    def get_terminal_velocity(self):
+        return np.sqrt((2 * self.mass * g) / (self.drag_coeff * air_density * self.cs_area))
+    
+    def create_object(self):
+        ball = sphere(pos=self.initial_pos, radius=self.vis_radius, color=color.red, make_trail=True)
+        ball.trail_color = self.trail_color
+        return ball
+    
+    def get_numerical_positions(self):
+        return (2 * self.mass) / (self.drag_coeff * air_density * self.cs_area) * \
+            np.log(np.cosh(np.sqrt(g * self.drag_coeff * air_density * self.cs_area / (2 * self.mass)) * self.time_steps)) * -1
+    
+    def get_numerical_velocities(self):
+        return np.sqrt(2 * self.mass * g / (self.drag_coeff * air_density * self.cs_area)) * \
+            np.tanh(np.sqrt(g * self.drag_coeff * air_density * self.cs_area /  (2 * self.mass)) * self.time_steps)
+    
+    def get_computed_velocities(self):
+        return np.gradient(self.comp_positions, dt)
+    
+    def plot_positions(self, title_suffix='', xlim=None, ylim=None):
+        num_positions = self.get_numerical_positions()
+
+        plt.plot(self.comp_positions, label='Computed Position', color='blue')
+        plt.plot(num_positions, label='Numerical Position', linestyle='--', color='green')
+        plt.title(f'Position vs Time ({title_suffix})')
+        plt.xlabel('Time Step (s)')
+        plt.ylabel('Position (m)')
+        if xlim:
+            plt.xlim(xlim)
+        if ylim:
+            plt.ylim(ylim)
+        plt.legend()
+
+    def plot_velocities(self, title_suffix='', xlim=None, ylim=None):
+        comp_velocities = self.get_computed_velocities()
+        num_velocities = self.get_numerical_velocities()
+
+        plt.plot(np.abs(comp_velocities), label='Computed Velocity', color='blue')
+        plt.plot(num_velocities, label='Numerical Velocity', linestyle='--', color='green')
+        plt.axhline(y=self.terminal_vel, color='red', linestyle=':', label='Terminal Velocity')
+        plt.title(f'Velocity vs Time ({title_suffix})')
+        plt.xlabel('Time Step (s)')
+        plt.ylabel('Velocity (m/s)')
+        if xlim:
+            plt.xlim(xlim)
+        if ylim:
+            plt.ylim(ylim)
+        plt.legend()
+
+    def convert_arrays(self):
+        self.comp_positions = np.array(self.comp_positions)
+        self.time_steps = np.array(self.time_steps)
+
+# Constants
 g = 9.81
-radius = 21.83 / 2 / 100
-mass = 7.26
-intial_pos = vector(0, 0, 0)
-final_pos = vector(0, -440, 0)
-initial_vel = vector(0, 0, 0)
-dt = 0.01  # time step
-drag_coeff = 0.5
 air_density = 1.225
-cs_area = np.pi * radius**2
-vis_scalar = 100
+dt = 0.1
 
-scene.autoscale = False
-scene.range = 2*abs(final_pos.y)
+bowling_ball = Ball(radius=21.83 / 2 / 100, mass=7.26, initial_pos=vector(-500, 0, 0), vis_scalar=200)
+basketball = Ball(radius=12 / 2 / 100, mass=0.625, initial_pos=vector(0, 0, 0), vis_scalar=200)
+golf_ball = Ball(radius=4.27 / 2 / 100, mass=0.04593, initial_pos=vector(500, 0, 0), vis_scalar=400)
 
-ball = sphere(pos=intial_pos, radius=radius*vis_scalar, color=color.red, make_trail=True)
-ball.trail_color = color.gray(0.5)
-ball.mass = mass
-ball.velocity = initial_vel
+balls = (bowling_ball, basketball, golf_ball)
 
-est_positions = []
-time_steps = []
+
 t = 0
+should_continue = True
+iter_count = 0
+max_iter = 500
 
-while ball.pos.y > final_pos.y:
-    F_drag = 0.5 * drag_coeff * air_density * cs_area * mag(ball.velocity)**2
-    F_grav = ball.mass * g
-    F = F_grav - F_drag
-
+while should_continue := any(round(mag(ball.velocity)) < round(ball.terminal_vel) for ball in balls):
     rate(60)
 
-    ball.acceleration = vector(0, -F / ball.mass, 0)
-    ball.velocity += ball.acceleration * dt
-    ball.pos += ball.velocity * dt
+    for ball in balls:
+        if round(mag(ball.velocity)) >= round(ball.terminal_vel):
+            continue
 
-    est_positions.append(ball.pos.y)
-    time_steps.append(t)
+        F_drag = 0.5 * ball.drag_coeff * air_density * ball.cs_area * mag(ball.velocity)**2
+        F_grav = ball.mass * g
+        F = F_grav - F_drag
+
+        ball.acceleration = vector(0, -F / ball.mass, 0)
+        ball.velocity += ball.acceleration * dt
+
+        ball.object.pos += ball.velocity * dt
+
+        ball.comp_positions.append(ball.object.pos.y)
+        ball.time_steps.append(t)
+
     t += dt
 
+    iter_count += 1
+    if iter_count >= max_iter:
+        break
 
-# Plotting the trajectory
-time_steps = np.array(time_steps)
-est_positions = np.array(est_positions)
-est_velocities = np.gradient(est_positions, dt)
+for ball in balls:
+    ball.convert_arrays()
 
-# Calcuating numerical positions and velocities for comparison
-num_positions = (2 * ball.mass) / (drag_coeff * air_density * cs_area) * \
-    np.log(np.cosh(np.sqrt(g * drag_coeff * air_density * cs_area / (2 * ball.mass)) * time_steps)) * -1
+max_terminal_vel = max(ball.terminal_vel for ball in balls)
+max_time = max(ball.time_steps[-1] for ball in balls) / dt
 
-num_velocities = np.sqrt(2 * ball.mass * g / (drag_coeff * air_density * cs_area)) * \
-    np.tanh(np.sqrt(g * drag_coeff * air_density * cs_area / (2 * ball.mass)) * time_steps)
+max_pos = max(abs(ball.comp_positions[-1]) for ball in balls)
 
-plt.figure(figsize=(12, 10))
-plt.subplot(2, 2, 1)
-plt.plot(est_positions, label='Estimated Position', color='green')
-plt.plot(num_positions, label='Numerical Position', linestyle='--', color='blue')
-plt.title('Y-position vs Time Step')
-plt.xlabel('Time Step (s)')
-plt.ylabel('Vertical Position (m)')
-plt.legend()
+xlim = (0, max_time*1.1)
+ylim_vel = (0, max_terminal_vel*1.1)
+ylim_pos = (-max_pos*1.1, 0)
 
-plt.subplot(2, 2, 2)
-plt.plot(np.abs(est_velocities), label='EstimatedVelocity', color='green')
-plt.plot(num_velocities, label='Numerical Velocity', linestyle='--', color='blue')
-plt.title('Velocity vs Time Step')
-plt.xlabel('Time Step (s)')
-plt.ylabel('Vertical Velocity (m/s)')
-plt.legend()
+plt.figure(figsize=(16, 10))
 
-abs_error_pos = np.abs(est_positions - num_positions)
-abs_error_vel = np.abs(est_velocities - num_velocities)
+plt.subplot(2, 3, 1)
+bowling_ball.plot_positions(title_suffix='Bowling Ball', xlim=xlim, ylim=ylim_pos)
 
-rel_error_pos = abs_error_pos / num_positions
-rel_error_vel = abs_error_vel / num_velocities
+plt.subplot(2, 3, 2)
+basketball.plot_positions(title_suffix='Basketball', xlim=xlim, ylim=ylim_pos)
 
-plt.subplot(2, 2, 3)
-plt.plot(rel_error_pos, label='Relative Error in Position', color='red')
-plt.title('Relative Error in Position vs Time Step')
-plt.xlabel('Time Step (s)')
-plt.ylabel('Relative Error')
-plt.legend()
+plt.subplot(2, 3, 3)
+golf_ball.plot_positions(title_suffix='Golf Ball', xlim=xlim, ylim=ylim_pos)
 
-plt.subplot(2, 2, 4)
-plt.plot(rel_error_vel, label='Relative Error in Velocity', color='red')
-plt.title('Relative Error in Velocity vs Time Step')
-plt.xlabel('Time Step (s)')
-plt.ylabel('Relative Error')
-plt.legend()
+plt.subplot(2, 3, 4)
+bowling_ball.plot_velocities(title_suffix='Bowling Ball', xlim=xlim, ylim=ylim_vel)
 
+plt.subplot(2, 3, 5)
+basketball.plot_velocities(title_suffix='Basketball', xlim=xlim, ylim=ylim_vel)
+
+plt.subplot(2, 3, 6)
+golf_ball.plot_velocities(title_suffix='Golf Ball', xlim=xlim, ylim=ylim_vel)
+
+plt.tight_layout(w_pad=6, h_pad=3)
 plt.show()
 
 
